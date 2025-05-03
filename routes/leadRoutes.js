b89927a0a009aa6c325e9e46ecf0806dfb5dd33d
Rahul Singh <rahuljsrasr1112@gmail.com>
2025-05-03 14:19:03 +0530

    mediaroute

--- routes/leadRoutes.js
+++ routes/leadRoutes.js
@@ -2,18 +2,19 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/leadController.js");
 const { protect, authorizeSuperAdmin } = require("../authMiddleware/authMiddleware.js");
+const { upload } = require("../authMiddleware/upload.js");
 
 router.post("/", controller.createLead);
 router.get("/", protect, authorizeSuperAdmin, controller.getAllLeads);
 router.get("/:id", controller.getLeadById);
 router.put("/status/:id", protect, authorizeSuperAdmin, controller.updateLeadStatus); // Only status
 router.delete("/:id", protect, authorizeSuperAdmin, controller.deleteLead);
-router.post(
-    "/create-salon-media/:userId",
-    upload.fields([
-      { name: "salonPhotos", maxCount: 10 },
-      { name: "salonAgreement", maxCount: 1 }
-    ]),
-    salonMediaController.createSalonMedia
+router.put(
+    "/update/media/:userId",
+  upload.fields([
+    { name: "salonPhotos", maxCount: 5 },
+    { name: "salonAgreement", maxCount: 1 },
+  ]),
+  controller.updateSalonMedia
   );
 module.exports = router;
